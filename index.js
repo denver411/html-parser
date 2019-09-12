0b67c459fc1f2472546bd5445c656608db379c50
Denis Zhebov <d.zhebov@semrush.com>
2019-09-12 16:55:33 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+const htmlString =
+  '<div class="root"><div class="r1 r1_1">content_1</div><div class="r1 r1_2"><div class="r2 r2_1">content_2_1</div><div class="r2 r2_2">content_2_2</div><div class="r2 r2_3">content_2_3</div><div class="r2 r2_4">content_2_4</div></div></div>';
